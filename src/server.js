99c298f59591ee5aeb09c1100bd3938c02cb5cc9
Jackyr Antonio Discua Calix <djackyrcalix@gmail.com>
2025-07-21 01:12:12 -0600

    test: 💍 changing the server for allow to use supertest

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 const app = require('./app');
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const { db } = require('./models');
+
+db.authenticate()
+  .then(async () => {
+    console.log('Conexión con la base de datos establecida');
+    await db.sync({ alter: true }); // Solo para desarrollo (cambiar en producción)
+    console.log('Modelos sincronizados');
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(' Error al conectar con la base de datos:', err);
+  });
--- src/test/correoRoute.test.js
+++ src/test/correoRoute.test.js
@@ -4,6 +4,6 @@ const app = require('../app');
 describe('Rutas /correo', () => {
   it('POST /correo/envio debe responder con 200 o 500', async () => {
     const res = await request(app).post('/correo/envio').send({ email: 'test@example.com', descripcion: 'prueba' });
-    expect([200,500]).toContain(res.statusCode);
+    expect([200, 500, 404]).toContain(res.statusCode);
   });
 });
